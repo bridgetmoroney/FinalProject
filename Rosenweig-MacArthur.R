a2abe3a5f29d541198162ab590d6054290921a7f
Bridget Moroney <bmoroney@nd.edu>
2018-11-28 09:00:51 -0500

    Adding Case 2 and 3

--- Rosenweig-MacArthur.R
+++ Rosenweig-MacArthur.R
@@ -30,6 +30,28 @@ modelSim1=ode(y=initial1,times=times1,func=ddSimRM,parms=params1)
 modelOutput1=data.frame(time=modelSim1[,1],H=modelSim1[,2],P=modelSim1[,3])
 ggplot(modelOutput1,aes(x=time,y=H))+geom_line()+theme_classic()+geom_line(aes(x=time,y=P,color="red"))
 
+#case 2 changing b
+#all params stay same except b=2.4
 
+params2=c(2.4,0.001,5,400,0.07,0.2)
+initial2=c(500,120)
+times2=seq(1,100, by=0.1)
+
+modelSim2=ode(y=initial2,times=times2,func=ddSimRM,parms=params2)
+modelOutput2=data.frame(time=modelSim2[,1],H=modelSim2[,2],P=modelSim2[,3])
+ggplot(modelOutput2,aes(x=time,y=H))+geom_line()+theme_classic()+geom_line(aes(x=time,y=P,color="red"))
+#shouldn't H increase if I multiplty it by 3?
+
+#case 3 changing e by decreasing
+#e is fifth parameter, new=0.035
+params3=c(0.8,0.001,5,400,0.0035,0.2)
+initial3=c(500,120)
+times3=seq(1,100, by=0.1)
+
+modelSim3=ode(y=initial3,times=times3,func=ddSimRM,parms=params3)
+modelOutput3=data.frame(time=modelSim3[,1],H=modelSim3[,2],P=modelSim3[,3])
+ggplot(modelOutput3,aes(x=time,y=H))+geom_line()+theme_classic()+geom_line(aes(x=time,y=P,color="red"))
+#This makes sense, if e is halved then predators are driven to extinction
+#because conversion efficiency so low
 
 
